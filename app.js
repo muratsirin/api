cb669040f9e44cd95de080f3d17ee2d75cf553ce
muratsirin <muratsirin.gm@gmail.com>
2021-12-06 19:59:44 +0300

    initial commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res){
-    res.send('Hello World');
+    res.send('Blogpost App Api');
 });
 
 app.listen(3000, function(){
